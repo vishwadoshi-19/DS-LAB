4054a1dbe311a097c5affcdcff6fd6ab517008f4
vishwadoshi-19 <99802964+vishwadoshi-19@users.noreply.github.com>
2022-11-16 03:05:32 +0530

    Create 4B_priority queue.c

---
+++ 4B_priority queue.c
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct item {
+	int value;
+	int priority;
+};
+
+item pr[100000];
+
+int size = -1;
+
+void enqueue(int value, int priority)
+{
+	
+	size++;
+
+	
+	pr[size].value = value;
+	pr[size].priority = priority;
+}
+
+int peek()
+{
+	int highestPriority = INT_MIN;
+	int ind = -1;
+
+	// Check for the element with
+	// highest priority
+	for (int i = 0; i <= size; i++) {
+
+		// If priority is same choose
+		// the element with the
+		// highest value
+		if (highestPriority == pr[i].priority && ind > -1
+			&& pr[ind].value < pr[i].value) {
+			highestPriority = pr[i].priority;
+			ind = i;
+		}
+		else if (highestPriority < pr[i].priority) {
+			highestPriority = pr[i].priority;
+			ind = i;
+		}
+	}
+
+	return ind;
+}
+
+void dequeue()
+{
+	
+	int ind = peek();
+
+	
+	for (int i = ind; i < size; i++) {
+		pr[i] = pr[i + 1];
+	}
+	size--;
+}
+
+int main()
+{
+	
+	enqueue(10, 2);
+	enqueue(14, 4);
+	enqueue(16, 4);
+	enqueue(12, 3);
+
+	int ind = peek();
+
+	cout << pr[ind].value << endl;
+
+	
+	dequeue();
+
+	ind = peek();
+	cout << pr[ind].value << endl;
+
+	dequeue();
+
+	ind = peek();
+	cout << pr[ind].value << endl;
+
+	return 0;
+}
