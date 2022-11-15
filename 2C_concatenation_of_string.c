53d79d06895305af8d26e30a7056c91ab6d81701
vishwadoshi-19 <99802964+vishwadoshi-19@users.noreply.github.com>
2022-11-16 00:15:02 +0530

    Create 2C_concatenation_of_string.c

---
+++ 2C_concatenation_of_string.c
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	char init[] = "this is init";
+	char add[] = " added now";
+
+	// concatenating the string.
+	strcat(init, add);
+
+	cout << init << endl;
+
+	return 0;
+}
